2f2449aee4be2632ff6501a58743ebb04e6ce4e2
CXFTEAMS <147907849+CXFTEAMS@users.noreply.github.com>
2023-10-28 15:50:31 +0700

    Cxfteams

---
+++ index.php
@@ -0,0 +1,85 @@
+<!-- Generator by NathanPrinsley -->
+<!-- Tools : https://script-deface-generator.prinsh.com -->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Hacked By CXFTEAMS</title>
+    <meta charset="UTF-8" />
+    <meta name="author" content="MR.KUCING" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="" />
+    <meta property="og:title" content="Hacked By MR.KUCING - CRIMINAL CYBER" />
+    <meta name="keywords" content="MR.KUCING - CRIMINAL CYBER" />
+    <meta property="og:image" content="https://cdn.prinsh.com/data-1/images/NathanPrinsley-AnonymousLogo.png" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.prinsh.com/NathanPrinsley-textstyle/nprinsh-stext.css" />
+    <style>
+        body {
+            background: #222222;
+            font-family: Calibri;
+            margin-top: 35px;
+        }
+
+        h1,
+        h2 {
+            margin-top: .3em;
+            margin-bottom: .3em;
+        }
+
+        h1.nprinsleyy {
+            color: #00FFFF;
+        }
+
+        h2 {
+            color: #dbd9d9;
+        }
+
+        p.message_prinsley {
+            color: #dbd9d9;
+            margin-top: .25em;
+            margin-bottom: .25em;
+            font-size: 16px;
+            font-weight: unset;
+        }
+
+        .hubungi_prinsh {
+            color: #00eb00;
+            text-decoration: none;
+        }
+
+        .hubungi_prinsh:hover {
+            color: red
+        }
+
+        .othermes_nprinsh {
+            color: #dbd9d9;
+            font-size: 16px;
+        }
+
+        marquee.foonathanPrinsley {
+            display: none;
+            position: fixed;
+            width: 100%;
+            bottom: 0px;
+            font-family: Tahoma;
+            height: 20px;
+            color: white;
+            left: 0px;
+            border-top: 2px solid darkred;
+            padding: 5px;
+            background-color: #000
+        }
+    </style>
+</head>
+
+<body>
+    <center />
+    <h1 class="nprinsleyy nprinsley-text-rainbowan" style="font-size:50px;">CXFTEAMS</h1>
+    <h2 style="font-size:24px;" class="nathan-prinsley_none">CRIMINAL CYBER</h2>
+    <h2 style="font-size:24px;" class="nathan-prinsley_none">Yang Kuat Kan Bertahan, Yang Lemah Berantakan.</h2>
+    </center>
+</body>
+
+</html><!-- Generator by Nathan Prinsley - https://script-deface-generator.prinsh.com -->

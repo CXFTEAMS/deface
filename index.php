27a218202f9044b7d1bc4eafe9e12054d13b411a
CXFTEAMS <147907849+CXFTEAMS@users.noreply.github.com>
2023-11-09 00:25:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>CXFTEAMS - criminal cyber</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 <style>
 body, html {
   height: 100%;

